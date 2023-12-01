cfb1d90efe4287a88c3571cd7f00212286543ce7
Jashwanth Pendyala <jp22226@corp.ojas-it.com>
2023-12-01 18:54:15 +0530

    Login

--- TaskManagement/lib/Login.dart
+++ TaskManagement/lib/Login.dart
@@ -1,25 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:taskmanagement/constants/AppConstants.dart';
 import 'package:taskmanagement/constants/BackgroundImage.dart';
-
-
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: BackgroundStack(
-        child:Card(child:
-          Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset("assets/images/logo.png")],
+        ),
+        SizedBox(
+          height: screenSize.height * 0.050,
+        ),
+        Container(
+          constraints: BoxConstraints(maxWidth: 320),
+          width: screenSize.width * 0.8, // 80% of screen width
+          height: screenSize.height * 0.65, // 60% of screen height
 
-            ],
-            )
-          ]),)
-      ),
+          child: Card(
+            color: AppConstants.cardLightColor,
+            shape: RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.circular(20.0), // Adjust the value as needed
+            ),
+            elevation: 5, // Add elevation for a shadow effect
+            child:const Padding(
+              padding:  EdgeInsets.all(18.0),
+              child:  Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding:  EdgeInsets.all(18.0),
+                        child: Text("Sign in",
+                            style: TextStyle(
+                                color: AppConstants.boldBlue,
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Align(
+                        alignment:Alignment.centerLeft,
+                        child: Text("Username",
+                            style: TextStyle(
+                              color: AppConstants.boldBlue,
+                              fontSize: 18
+                            )),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ])),
     );
   }
 }
--- TaskManagement/lib/main.dart
+++ TaskManagement/lib/main.dart
@@ -1,36 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:taskmanagement/LandingPage.dart';
+import 'package:taskmanagement/Login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Login(),));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            Image.asset(
-              "assets/images/bg.jpg",
-              fit: BoxFit.cover,
-            ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Your other content here
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+
